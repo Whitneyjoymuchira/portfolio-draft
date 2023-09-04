3e6892aa6a046bef299727d4267fc3b37f3d4f02
Whitneyjoymuchira <101453939+Whitneyjoymuchira@users.noreply.github.com>
2023-09-04 22:28:30 +0300

    portfolio reedit

---
+++ src/data/data.jsx
@@ -0,0 +1,30 @@
+
+import Traveller from "../assets/projects/travellers.jpg";
+import GymImg from '../assets/projects/gym.png';
+import newsLetter from "../assets/projects/newsletter.png";
+export const data=[
+    {
+        id:1,
+        name:"Travellers app",
+        image:Traveller,
+        github:"https://github.com/Whitneyjoymuchira/Travellers",
+        live:"https://tour-travellers.netlify.app",
+    },
+    {
+        id:2,
+        name:"Gym Workouts Application",
+        image:GymImg,
+        github:"https://github.com/Whitneyjoymuchira/gym-app",
+        live:"https://bodygym.netlify.app",
+    },
+    {
+        id:3,
+        name:"React Dictionary Application",
+        image:newsLetter,
+        github:"https://github.com/Whitneyjoymuchira/ReactJs-Dictionary",
+        live:"https://reactjs-dictionaryapp.netlify.app",
+    },
+   
+
+
+]
